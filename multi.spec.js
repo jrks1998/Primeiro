ac34630f7d396959fb623f9495bcc0ad4c0a61ea
jrks1998 <jonas.schneider@institutoivoti.com.br>
2017-03-29 00:29:27 -0300

    novos testes

--- multi.spec.js
+++ multi.spec.js
@@ -4,38 +4,21 @@ const lab = exports.lab = Lab.script();
 
 const { multi } = require('./multi');
 
-lab.test('retorno da soma de 1 e 3 deve ser 4', (done) => {
+lab.test('soma numeros inteiros', (done) => {
 
     Code.expect(multi(1, 3)).to.equal(4);
+    Code.expect(multi(18, 51)).to.equal(69);
     done();
 });
 
-lab.test('a soma -8 e 9 deve ser igual a 1', (done) => {
-
-    Code.expect(multi(-8, 9)).to.equal(1);
-    done();
-});
-
-lab.test('a soma -8 e -9 deve ser igual a -17', (done) => {
+lab.test('somando numeros negativos', (done) => {
 
     Code.expect(multi(-8, -9)).to.equal(-17);
     done();
 });
 
-lab.test('retorno da soma de h e 3 deve ser 3', (done) => {
-
-    Code.expect(multi(h, 3)).to.equal(3);
-    done();
-});
-
-lab.test('retorno da soma de 1 e nada deve ser 1', (done) => {
-
-    Code.expect(multi(1)).to.equal(1);
-    done();
-});
-
-lab.test('retorno da soma de nada e nada deve ser nada', (done) => {
-
-    Code.expect(multi(0, 0)).to.equal(0);
+lab.test('expressão mal formada', (done) => {
+    const r = multi();
+    Code.expect(r.message).to.equal("parametros faltando")
     done();
 });
